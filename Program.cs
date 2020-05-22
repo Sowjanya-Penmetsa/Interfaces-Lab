e5f95b4645662826dd7e3e2079ae64a17dad9bf7
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-22 15:48:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Interfaces_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the radius");
+            var radius = int.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(radius);
+            Console.WriteLine("enter the width and height");
+
+            var width = int.Parse(Console.ReadLine());
+
+            var height = int.Parse(Console.ReadLine());
+
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+
+            rect.Draw();
+            Console.ReadLine();
+        }
+    }
+}
